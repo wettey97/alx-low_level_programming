4325dbd58d8f72de36254aeffac98f9c78a3cdff
Elijah Wettey <wettey97@gmail.com>
2023-06-18 15:33:18 +0100

    Adding my frst Cprogramming files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * Main - Entry point
+ * main - Entry point
  * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
